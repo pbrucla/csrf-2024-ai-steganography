4f3c9d277372bc153bc6d07f3b7fe3d21ad4fe77
leon <leonlenk@gmail.com>
2024-08-02 06:18:22 +0000

    update bash scripts

--- custom_stego_implement/Test_create_stego.sh
+++ custom_stego_implement/Test_create_stego.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-for i in $(ls data/CustomStego/1kFromcleanTest/); do python customstego_implement/hamming_codes_random_bit.py data/CustomStego/1kFromcleanTest/$i 120000 data/CustomStego/hamming_codes_binary_Test/120k_HAMMING$i; done
+for i in $(ls data/CustomStego/1kFromcleanTest/); do poetry run python custom_stego_implement/hamming_codes_random_bit.py data/CustomStego/1kFromcleanTest/$i 120000 data/CustomStego/hamming_codes_binary_Test/120k_HAMMING$i; done &
 
-python custom_stego_implement/LSB_random_bit.py data/CustomStego/1kFromcleanTest/ 120000 data/CustomStego/LSBTest/
+poetry run python custom_stego_implement/LSB_random_bit.py data/CustomStego/1kFromcleanTest/ 120000 data/CustomStego/LSBTest/ &
 
-python custom_stego_implement/PVD_random_bit.py data/CustomStego/2kFromcleanTest/ 120000 data/CustomStego/PVDTest/
+poetry run python custom_stego_implement/PVD_random_bit.py data/CustomStego/2kFromcleanTest/ 120000 data/CustomStego/PVDTest/
