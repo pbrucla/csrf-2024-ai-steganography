9e51d7542f24d35e5c94cac25b1c3ecebd0df06a
Asher Christian <asher.christian@gmail.com>
2024-08-01 22:15:46 -0700

    new custom random bit implementations

---
+++ custom_stego_implement/LSB_random_bit.py
@@ -0,0 +1,102 @@
+import cv2
+import numpy as np
+import random
+import os
+import sys
+
+
+
+
+#TXT_PATH = sys.argv[2]
+
+
+VERBOSE = True
+ENCODE = True
+EXTRACT = False
+
+
+
+def lsb_hide_rgb(image : str, secret_data: str):
+    secret_bin = secret_data
+    data_index = 0
+    secret_data_len = len(secret_bin)
+    
+    channels = cv2.split(image)
+    for channel in channels:
+        rows, cols = channel.shape
+        for i in range(rows): # skip a row? maybe change to just 1
+            for j in range(cols): 
+                if data_index < secret_data_len:
+                    p1 = channel[i, j]
+                    p1 = p1 & 0xFE
+                    p1 = p1 | int(secret_bin[data_index],2)
+                    channel[i, j] = p1
+                    data_index += 1
+    return cv2.merge(channels)
+
+def lsb_extract_rgb(stego_image, secret_len):
+    channels = cv2.split(stego_image)
+    extracted_bin = ''
+    
+    for channel in channels:
+        rows, cols = channel.shape
+        for i in range(rows):
+            for j in range(cols):
+                if len(extracted_bin) < secret_len * 8:
+                    p1 = channel[i, j]
+                    extracted_bin += str(p1 & 1)
+    secret_data = ''.join(chr(int(extracted_bin[i:i+8], 2)) for i in range(0, len(extracted_bin), 8))
+    return extracted_bin, secret_data[:secret_len]
+
+# Load an example image
+
+
+
+def binary_to_string(binary_str):
+    chars = [binary_str[i:i+8] for i in range(0, len(binary_str), 8)]
+    ascii_str = ''.join(chr(int(char, 2)) for char in chars)
+    return ascii_str
+
+
+
+
+def main():
+    if len(sys.argv) != 4:
+        print("Usage: python t.py <input_dir> <encode_file> <destination_dir>")
+        sys.exit(1)
+
+    source_dir = sys.argv[1]
+    destination_dir = sys.argv[3]
+    message_length = int(sys.argv[2])
+
+
+    # Get the list of files in the source directory
+    source_files = os.listdir(source_dir)
+    print(len(source_files))
+    for i, file_name in enumerate(source_files):
+        src_path = os.path.join(source_dir, file_name)
+        file_name = str(message_length)[:-3] + "k_LSB_" + file_name
+        dest_path = os.path.join(destination_dir, file_name)
+        if(ENCODE):
+            image_path = src_path
+            image = cv2.imread(image_path) # -> numpy array (w, h, channels)
+
+            # Define secret data to hide
+            secret_data = ''.join(str(random.randint(0,1)) for _ in range(message_length * 8))
+            # Hide secret data in the image using PVD
+            stego_image = lsb_hide_rgb(image, secret_data)
+
+            # Save the stego image
+            stego_image_path = dest_path
+            cv2.imwrite(stego_image_path, stego_image)
+            print(f"written {message_length} bytes in  {stego_image_path} from {image_path}, number: {i}")
+
+        # Extract the secret data from the stego image
+        if(EXTRACT):
+            bin, extracted_data = lsb_extract_rgb(stego_image, len(secret_data))
+
+
+
+if __name__ == "__main__":
+
+    main()
---
+++ custom_stego_implement/hamming_codes_random_bit.py
@@ -0,0 +1,256 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2020 Daniel Lerch Hostalot. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a 
+# copy of this software and associated documentation files (the "Software"), 
+# to deal in the Software without restriction, including without limitation 
+# the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+# and/or sell copies of the Software, and to permit persons to whom the 
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in 
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+# DEALINGS IN THE SOFTWARE.
+
+
+# Matrix Embedding using binary Hamming codes.
+EXTRACT = False
+EMBED = True
+VERBOSE = True
+
+import sys
+import random
+import numpy as np
+import os
+import random
+
+
+class HC:
+    def __init__(self, p, min_value=0, max_value=255):
+        # {{{
+        self.code = 2 # binary
+        self.msg_len = p
+        self.block_len = self.code**self.msg_len-1
+        self.gen_H()
+        self.max_value=max_value
+        self.min_value=min_value
+        # }}}
+ 
+    def _n_ary(self, n):
+        # {{{
+        if n == 0:
+            return '0'
+        nums = []
+        while n:
+            n, r = divmod(n, self.code)
+            nums.append(str(r))
+        return ''.join(reversed(nums))
+        # }}}
+
+    def gen_H(self):
+        # {{{
+        H = []
+        l = len(self._n_ary(self.block_len))
+        for i in range(1, self.code**self.msg_len):
+            string = self._n_ary(i).zfill(l)
+            V=[int(c) for c in string]
+            H.append(V)
+        self.H=np.array(H).T
+        # }}}
+
+    def _bytes_to_bits(self, m):
+        # {{{
+        bits=[]
+        for b in m:
+            for i in range(8):
+                bits.append((b >> i) & 1)
+        return np.array(bits)
+        # }}}
+    
+    def _bits_to_bytes(self, m):
+        # {{{
+        enc = bytearray()
+        idx=0
+        bitidx=0
+        bitval=0
+        for b in m:
+            if bitidx==8:
+                enc.append(bitval)
+                bitidx=0
+                bitval=0
+            bitval |= b<<bitidx
+            bitidx+=1
+        if bitidx==8:
+            enc.append(bitval)
+
+        return bytes(enc)
+        # }}}
+
+    def _embed(self, c, m):
+        # {{{
+
+        s = c.copy()
+        i=j=0
+        while True:
+            m_chunk = m[i:i+self.msg_len]
+            c_chunk = c[j:j+self.block_len]%self.code
+
+
+            if len(m_chunk) == 0:
+                break
+
+            if len(m_chunk) != self.msg_len:
+                print("ERROR: wrong message length:", m_chunk)
+                sys.exit(0)
+
+            if len(c_chunk)!=self.block_len:
+                #print("ERROR: wrong cover length or message too long:", c_chunk)
+                break
+                #sys.exit(0)
+
+            column = (self.H.dot(c_chunk)-m_chunk)%self.code
+
+            # Find position of column in H
+            position = np.where(np.sum(np.abs(self.H.T-column), axis=1)==0)[0]
+
+            if len(position)>0:
+                v = s[j:j+self.block_len][position[0]]
+                add = random.choice([1, -1])
+                if v == self.max_value:
+                    add = -1
+                elif v == self.min_value:
+                    add = 1
+                s[j:j+self.block_len][position[0]] += add
+
+            i += self.msg_len
+            j += self.block_len
+
+        return s
+        # }}}
+
+    def embed(self, cover, message):
+        # {{{
+        shape = cover.shape
+        c = cover.flatten()
+
+        # Insert message length
+        l = len(message)
+        msg_len = l.to_bytes(4, 'big')
+
+        m = self._bytes_to_bits(msg_len)
+        # Left padding
+        while len(m) % self.msg_len != 0:
+            m = np.insert(m, 0, 0)
+
+        c_len =(len(m)//self.msg_len)*self.block_len
+        s_header = self._embed(c[:c_len], m)
+
+
+        # Insert the message
+        m = self._bytes_to_bits(message)
+
+        # Right padding
+        while len(m)%self.msg_len != 0:
+            m = np.append(m, 0)
+
+        s_content = self._embed(c[c_len:], m)
+
+        s = np.append(s_header, s_content, axis=0)
+
+        return s.reshape(shape)
+        # }}}
+
+    def _extract(self, s):
+        # {{{ 
+        m = []
+
+        for i in range(0, len(s), self.block_len):
+            s_chunk = s[i:i+self.block_len]%self.code
+            if len(s_chunk)<self.block_len:
+                break
+            m_chunk = self.H.dot(s_chunk)%self.code
+            m += m_chunk.tolist()
+
+        return m
+        # }}}
+
+    def extract(self, stego):
+        # {{{
+        shape = stego.shape
+        s = stego.flatten()
+
+        # Read header
+        l = 4*8
+        while l%self.msg_len != 0:
+            l += 1
+
+        bytes_len =(l//self.msg_len)*self.block_len
+
+        m = self._extract(s[:bytes_len])
+        while (len(m))%8 != 0:
+            m = m[1:]
+
+        b = self._bits_to_bytes(m)
+        msg_len = int.from_bytes(b, 'big') 
+
+        # Read message
+        m = self._extract(s[bytes_len:])
+        while len(m)%8 != 0:
+            m.pop()
+
+
+        message_bytes = self._bits_to_bytes(m[:msg_len*8])
+
+        return message_bytes
+        # }}}
+
+
+# Example
+if __name__ == "__main__":
+
+    import imageio.v2 as imageio
+
+    OutputImage = sys.argv[1]
+    hc = HC(3)
+    if EMBED:
+        InputImage = sys.argv[1]
+        message_length = int(sys.argv[2])
+        OutputImage = sys.argv[3][:-4] + '.png'
+        cover = imageio.imread(InputImage)
+        if os.path.isfile(OutputImage):
+            print(f'skip {OutputImage}')
+            sys.exit()
+        if not(len(cover.shape) == 3 and cover.shape[2] == 3) and InputImage[-4:] != '.png':
+            print('not rgb', InputImage)
+            sys.exit()
+
+        
+        message = bytes([random.getrandbits(8) for _ in range(message_length)])
+        
+
+        stego = cover.copy()
+        stego[:,:,0] = hc.embed(cover[:,:,0], message)
+
+        imageio.imsave(OutputImage, stego)
+        print(f"hid {message_length} random bytes in {OutputImage} from {InputImage}")
+        if(VERBOSE):
+            print(f"Cover:\n{cover[:10,:10,0]}\n")
+            print(f"Stego:\n{stego[:10,:10,0]}\n")
+    if(EXTRACT):
+        stego = imageio.imread(OutputImage)
+        extracted_message = hc.extract(stego[:,:,0])
+        print("Extracted message:", extracted_message.decode())
+        
+
+
+
+
+
