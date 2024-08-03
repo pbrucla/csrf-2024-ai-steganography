6de5b4c6db91d7e224e0db0b6e170e09c5cf885d
leon <leonlenk@gmail.com>
2024-08-03 01:04:48 +0000

    change bash scripts and update poetry

--- custom_stego_implement/Val_create_stego.sh
+++ custom_stego_implement/Val_create_stego.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-for i in $(ls data/CustomStego/2kFromcleanVal/); do poetry run python custom_stego_implement/hamming_codes_random_bit.py data/CustomStego/2kFromcleanVal/$i 120000 data/CustomStego/hamming_codes_binary_Val/120k_HAMMING$i; done &
 
-poetry run python custom_stego_implement/LSB_random_bit.py data/CustomStego/2kFromcleanVal/ 120000 data/CustomStego/LSBVal/ &
+mkdir -p data/CustomStego/hamming_codes_binary_Val
+mkdir -p data/CustomStego/LSBVal
+mkdir -p data/CustomStego/PVDVal
+
+for i in $(find data/CustomStego/2kFromcleanVal/ -type f); do
+    [ -e "$i" ] || continue
+    filename=$(basename "$i")
+    poetry run python custom_stego_implement/hamming_codes_random_bit.py "$i" 120000 "data/CustomStego/hamming_codes_binary_Val/$filename"
+done
+
+poetry run python custom_stego_implement/LSB_random_bit.py data/CustomStego/2kFromcleanVal/ 120000 data/CustomStego/LSBVal/ 
 poetry run python custom_stego_implement/PVD_random_bit.py data/CustomStego/2kFromcleanVal/ 120000 data/CustomStego/PVDVal/
+
+wait
---
+++ custom_stego_implement/test.sh
@@ -0,0 +1,3 @@
+for i in data/CustomStego/cleanTrain/*; do
+    echo $(basename "$i")
+done
