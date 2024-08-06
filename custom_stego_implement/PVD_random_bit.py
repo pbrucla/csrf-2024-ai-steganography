fixing overflow and underflow issues

custom_stego_implement/PVD_random_bit.py
@@ -19,15 +19,17 @@ ENCODE = True
 EXTRACT = False
 
 def f(p1, p2, m):
+    p1 = p1.astype(np.int64)
+    p2 = p2.astype(np.int64)
     ceiling = math.ceil(m/2.0)
     floor = math.floor(m/2.0)
     d = abs(int(p1) - int(p2))
     if (d%2) == 1:
-        g1 = p1+np.array(-ceiling).astype('uint8')
-        g2 = p2+np.array(floor).astype('uint8')
+        g1 = p1-ceiling
+        g2 = p2+floor
     else:
-        g1 = p1+np.array(-floor).astype('uint8')
-        g2 = p2+np.array(ceiling).astype('uint8')
+        g1 = p1-floor
+        g2 = p2+ceiling
     return g1, g2
 
 
@@ -61,7 +63,7 @@ def pvd_hide_rgb(image : str, secret_data: str):
                     else:
                         dprime = -(lowers[k]+b)
                     test = f(p1, p2, uppers[k] - d)
-                    if(test[0] > 255 or test[1] > 255):
+                    if(test[0] > 255 or test[1] > 255 or test[0] < 0 or test[1] < 0):
                         continue
                     data_index += n
                     m = dprime - d